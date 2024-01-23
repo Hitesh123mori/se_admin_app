b3568758065a7e927e3ab730a2f129046a0588c9
Niraj kc <niraj.kc.128@gmail.com>
2024-01-23 20:25:08 +0530

    Primary info done

--- lib/Providers/InfoProvider.dart
+++ lib/Providers/InfoProvider.dart
@@ -1,14 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
+import 'package:se_admin_app/apis/FirebaseAPI.dart';
 
 class InfoProvider extends ChangeNotifier{
+  final _colRef = FirebaseAPI.dbAPI.collection("primaryInfo");
 
   Map<String, bool> options = {
-    "Director Information": true,
-    "About Us": false,
+    "About Us": true,
+    "Vision": false,
+    "Mission": false,
+    "Director Information": false,
     "Contact Us": false,
   };
 
-  String current = "Director Information";
+  String current = "About Us";
+
+  Map<String, TextEditingController> infoTEC = {};
 
   void updateCurrent(String option){
     options[current] = false;
@@ -16,4 +23,26 @@ class InfoProvider extends ChangeNotifier{
     options[current] = true;
     notifyListeners();
   }
+
+  String getKey() {
+    return current.toLowerCase().replaceAll(" ", "_");
+  }
+
+  Future<void> update(String key, String value)async {
+    final docString = getKey();
+
+    await _colRef.doc(docString).update({key: value});
+
+  }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getInfo() async{
+    final docString = current.toLowerCase().replaceAll(" ", "_");
+
+    return await _colRef.doc(docString).get().then((value) => value);
+  }
+
+  void notify(){
+    notifyListeners();
+  }
+
 }
